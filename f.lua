53705b4eb4c7f46c32521127369bd87f2c7734ba
heap <68669985+heap-v@users.noreply.github.com>
2022-08-30 16:54:29 +0100

    Create f.lua

---
+++ f.lua
@@ -0,0 +1,370 @@
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/detect.lua", true))()
+
+if game.PlaceId == 10118559731 then
+local DiscordLib =
+    loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+local win = DiscordLib:Window("Flerix Hub - heap#3866 + s15_#0338")
+
+local serv = win:Server("Flerix Hub", "http://www.roblox.com/asset/?id=10625452220") -- Create new server
+
+local lbls = serv:Channel("Credits") -- New channel in server
+
+
+lbls:Label("Flerix Hub by heap#3866 & s15_#0338")
+lbls:Label("Brought to you by Wanted Studio")
+
+local btns = serv:Channel("Discord Support") -- New channel in server
+
+btns:Button(
+    "Flerixhub",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/Discord2.lua", true))()
+        DiscordLib:Notification("Notification", "Joined!", "ty.")
+    end
+)
+btns:Button(
+    "Wanted Studio",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/DiscordInvite.lua", true))()
+        DiscordLib:Notification("Notification", "Joined!", "ty.")
+    end
+)
+
+local serv = win:Server("Nicos Nextbot", "http://www.roblox.com/asset/?id=10625450160") -- Create new server
+
+local btns = serv:Channel("Functions") -- New channel in server
+
+btns:Button(
+    "Powerbox ESP + Bot ESP",
+    function()
+    loadstring(game:HttpGet("https://fern.wtf/scripts/nico_bot.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Godmode",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/Godmode.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "SEMI-Godmode",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/Semi-Godmode.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Open ALL Doors",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/Openalldoors.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Fullbright",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/FB.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Auto BHop / Hold Space",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/AutoBHop.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+-- SPECTER SCRIPT ⬇⬇⬇⬇
+-- SPECTER SCRIPT ⬇⬇⬇⬇
+-- SPECTER SCRIPT ⬇⬇⬇⬇
+elseif game.PlaceId == 5911084042 then
+    local DiscordLib =
+        loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+    
+    local win = DiscordLib:Window("Flerix Hub - heap#3866 + s15_#0338", "Nicos Nextbot")
+    
+    local serv = win:Server("Flerix Hub", "") -- Create new server
+
+    local lbls = serv:Channel("Credits") -- New channel in server
+
+    lbls:Label("Flerix Hub by heap#3866 & s15_#0338")
+    lbls:Label("Brought to you by Wanted Studio")
+    
+    local btns = serv:Channel("Discord Support") -- New channel in server
+    
+    btns:Button(
+        "Support",
+        function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/DiscordInvite.lua", true))()
+            DiscordLib:Notification("Notification", "Joined!", "ty.")
+        end
+    )
+
+local serv = win:Server("Specter", "")
+
+local btns = serv:Channel("Functions")
+
+btns:Button(
+    "Fullbright",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/NicosNextBot/main/FB.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Ghost ESP",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/GESP.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Unlock ALL Gamepasses",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/SPECUNAG.lua", true))()
+        DiscordLib:Notification("Notification", "THIS SCRIPT IS NOT WORKING / UNSAFE!", "Ok.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Check Ghost Orbs",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/CheckGO.lua", true))()
+        DiscordLib:Notification("Notification", "THIS SCRIPT IS NOT WORKING / UNSAFE!", "Ok.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Never Be Alone",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/NeverBA.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Infinite Sanity",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/InfSan.lua", true))()
+        DiscordLib:Notification("Notification", "Done!", "ty.")
+    end
+)
+
+-- PRESSURE WASH SCRIPT ⬇⬇⬇⬇
+-- PRESSURE WASH SCRIPT ⬇⬇⬇⬇
+-- PRESSURE WASH SCRIPT ⬇⬇⬇⬇
+elseif game.PlaceId == 7009799230 then
+    local DiscordLib =
+        loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+    
+    local win = DiscordLib:Window("Flerix Hub - heap#3866 + s15_#0338", "Nicos Nextbot")
+    
+    local serv = win:Server("Flerix Hub", "") -- Create new server
+
+    local lbls = serv:Channel("Credits") -- New channel in server
+
+    lbls:Label("Flerix Hub by heap#3866 & s15_#0338")
+    lbls:Label("Brought to you by Wanted Studio")
+    
+    local btns = serv:Channel("Discord Support") -- New channel in server
+
+    btns:Button(
+        "Support",
+        function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/DiscordInvite.lua", true))()
+            DiscordLib:Notification("Notification", "Joined!", "ty.")
+        end
+    )
+
+local serv = win:Server("Pressure Wash Simulator", "") -- Create new server
+
+local tgls = serv:Channel("main") -- New channel in server
+
+tgls:Toggle("Infinite Money",
+    false,
+    function(bool)
+        getgenv().infmoney = bool
+    if bool then
+        getgenv().infmoney = bool
+    while getgenv().infmoney == true do
+    wait(.1)
+    game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(math.huge, math.huge)
+    game:GetService("ReplicatedStorage").Remotes.ClientFrameData:FireServer(math.huge , true, true, math.huge)
+end
+        end
+end)
+
+tgls:Seperator()
+
+tgls:Toggle("Infinite Water",
+    false,
+    function(bool)
+        getgenv().infwater = bool -- [name] = id
+        print('water is: ', bool);
+    if bool then
+        getgenv().infwater = bool
+    while getgenv().infwater == true do
+        wait(.1)
+    game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(true) 
+end
+    end
+end)
+
+else 
+    local DiscordLib =
+    loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+local win = DiscordLib:Window("Flerix Hub - heap#3866 + s15_#0338")
+
+local serv = win:Server("Flerix Hub", "http://www.roblox.com/asset/?id=10625452220") -- Create new server
+
+local lbls = serv:Channel("Credits") -- New channel in server
+
+
+lbls:Label("Flerix Hub by heap#3866 & s15_#0338")
+lbls:Label("Brought to you by Wanted Studio")
+
+local btns = serv:Channel("Discord Support") -- New channel in server
+
+btns:Button(
+    "Support",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/Discord2.lua", true))()
+        DiscordLib:Notification("Notification", "Joined!", "ty.")
+    end
+)
+btns:Button(
+    "Wanted Studio",
+    function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/Discord2.lua", true))()
+        DiscordLib:Notification("Notification", "Joined!", "ty.")
+    end
+)
+end
+
+-- CIM SCRIPT ⬇⬇⬇⬇
+-- CIM SCRIPT ⬇⬇⬇⬇
+-- CIM SCRIPT ⬇⬇⬇⬇
+if game.PlaceId == 4588604953 then
+    local DiscordLib =
+        loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+    
+    local win = DiscordLib:Window("Flerix Hub - heap#3866 + s15_#0338", "")
+    
+    local serv = win:Server("Flerix Hub", "") -- Create new server
+
+    local lbls = serv:Channel("Credits") -- New channel in server
+
+    lbls:Label("Flerix Hub by heap#3866 & s15_#0338")
+    lbls:Label("Brought to you by Wanted Studio")
+    
+    local btns = serv:Channel("Discord Support") -- New channel in server
+
+    btns:Button(
+        "Support",
+        function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/DiscordInvite.lua", true))()
+            DiscordLib:Notification("Notification", "Joined!", "ty.")
+        end
+    )
+
+local serv = win:Server("Criminality"), ("") -- Create new server
+
+local tgls = serv:Channel("General") -- New channel in server
+
+tgls:Toggle("Aimbot"),
+loadstring(game:HttpGet("https://pastebin.com/rpM0k2PN", true))()
+
+ = tgls:Seperator()
+  
+tgls:Toggle("Scrap ESP"),
+loadstring(game:HttpGet("https://raw.githubusercontent.com/heap-v/FlerixHub/main/zzz.lua", true))()
+
+    tgls:Toggle("Tool ESP",
+for __,v in pairs(game.Workspace.Filter.SpawnedTools:GetChildren()) do
+if v:IsA("Model") or v:IsA("Part") or v:IsA("MeshPart") then
+local a = Instance.new("BillboardGui",v)
+a.Size = UDim2.new(1,0, 1,0)
+a.Name = "A"
+a.AlwaysOnTop = true
+local b = Instance.new("Frame",a)
+b.Size = UDim2.new(1,0, 1,0)
+b.BackgroundTransparency = 1
+b.BorderSizePixel = 0
+local c = Instance.new("TextLabel",b)
+c.Text = v.Name
+c.Size = UDim2.new(1,0, 1,0)
+c.BackgroundTransparency = 1
+c.BorderSizePixel = 0
+end
+end
+          
+    tgls:Toggle("Cash ESP",
+for __,v in pairs(game.Workspace.Filter.SpawnedCash:GetChildren()) do
+if v:IsA("Model") or v:IsA("Part") or v:IsA("MeshPart") then
+local a = Instance.new("BillboardGui",v)
+a.Size = UDim2.new(1,0, 1,0)
+a.Name = "A"
+a.AlwaysOnTop = true
+local b = Instance.new("Frame",a)
+b.Size = UDim2.new(1,0, 1,0)
+b.BackgroundTransparency = 1
+b.BorderSizePixel = 0
+local c = Instance.new("TextLabel",b)
+c.Text = v.Name
+c.Size = UDim2.new(1,0, 1,0)
+c.BackgroundTransparency = 1
+c.BorderSizePixel = 0
+end
+end
+   
+ tgls:Toggle("Register ESP",        
+for __,v in pairs(game.Workspace.Map.CashRegisters:GetChildren()) do
+if v:IsA("Model") or v:IsA("Part") or v:IsA("MeshPart") then
+local a = Instance.new("BillboardGui",v)
+a.Size = UDim2.new(1,0, 1,0)
+a.Name = "A"
+a.AlwaysOnTop = true
+local b = Instance.new("Frame",a)
+b.Size = UDim2.new(1,0, 1,0)
+b.BackgroundTransparency = 1
+b.BorderSizePixel = 0
+local c = Instance.new("TextLabel",b)
+c.Text = v.Name
+c.Size = UDim2.new(1,0, 1,0)
+c.BackgroundTransparency = 1
+c.BorderSizePixel = 0
+end
+end
+
+tgls:Seperator()
